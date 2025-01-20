5498aded51a39c321eba81ddacda34f9f4ab352b
Tim Liang <timliang@Tims-MacBook-Pro-5.local>
2025-01-19 19:50:15 -0500

    Update ApplicationRegistry by adding orgsToFormIds and new ObjectId mappings

--- src/main/Form/ApplicationRegistry.java
+++ src/main/Form/ApplicationRegistry.java
@@ -6,6 +6,8 @@ import org.json.JSONObject;
 
 import java.math.BigDecimal;
 import java.util.Optional;
+import java.util.Map;
+import java.util.HashMap;
 
 public enum ApplicationRegistry {
   SS$FED$INITIAL(
@@ -15,7 +17,20 @@ public enum ApplicationRegistry {
       ApplicationSubtype.INITIAL,
       BigDecimal.ZERO,
       1,
-      new ObjectId("123400000000000000000000")),
+      new HashMap<String,ObjectId>()),
+
+  // Fields that need to be verified: Application Subtype, amount, numWeeks
+  SS$FED$REPLACEMENT(
+      IdCategoryType.SOCIAL_SECURITY_CARD,
+      USStateOrCity.FED,
+      Optional.empty(),
+      ApplicationSubtype.DUPLICATE,
+      BigDecimal.ZERO,
+      1,
+      new HashMap<String,ObjectId>(){{
+        this.put("Face to Face", new ObjectId("6725daa1ebfdb30698fff327"));
+        this.put("TSA C.A.T.S Program", new ObjectId("672871ed2de24d7c8ba75c30"));
+      }}),
 
   BC$PA$INITIAL(
       IdCategoryType.BIRTH_CERTIFICATE,
@@ -24,7 +39,9 @@ public enum ApplicationRegistry {
       ApplicationSubtype.INITIAL,
       BigDecimal.ZERO,
       1,
-      new ObjectId("123400000000000000000000")),
+      new HashMap<String,ObjectId>(){{
+        this.put("TSA C.A.T.S Program", new ObjectId("67206e7e17d3b63a60456d45"));
+      }}),
   BC$PA$DUPLICATE(
       IdCategoryType.BIRTH_CERTIFICATE,
       USStateOrCity.PA,
@@ -32,7 +49,7 @@ public enum ApplicationRegistry {
       ApplicationSubtype.DUPLICATE,
       new BigDecimal("20.0"),
       1,
-      new ObjectId("123400000000000000000000")),
+          new HashMap<String,ObjectId>()),
   BC$PA$HOMELESS(
       IdCategoryType.BIRTH_CERTIFICATE,
       USStateOrCity.PA,
@@ -40,7 +57,7 @@ public enum ApplicationRegistry {
       ApplicationSubtype.HOMELESS,
       BigDecimal.ZERO,
       1,
-      new ObjectId("123400000000000000000000")),
+          new HashMap<String,ObjectId>()),
   BC$PA$JUVENILE_JUSTICE_INVOLVED(
       IdCategoryType.BIRTH_CERTIFICATE,
       USStateOrCity.PA,
@@ -48,7 +65,7 @@ public enum ApplicationRegistry {
       ApplicationSubtype.JUVENILE_JUSTICE_INVOLVED,
       BigDecimal.ZERO,
       1,
-      new ObjectId("123400000000000000000000")),
+          new HashMap<String,ObjectId>()),
   BC$PA$VETERANS(
       IdCategoryType.BIRTH_CERTIFICATE,
       USStateOrCity.PA,
@@ -56,7 +73,7 @@ public enum ApplicationRegistry {
       ApplicationSubtype.VETERANS,
       BigDecimal.ZERO,
       1,
-      new ObjectId("123400000000000000000000")),
+      new HashMap<String,ObjectId>()),
   PIDL$PA$PI$INITIAL(
       IdCategoryType.DRIVERS_LICENSE_PHOTO_ID,
       USStateOrCity.PA,
@@ -64,7 +81,9 @@ public enum ApplicationRegistry {
       ApplicationSubtype.INITIAL,
       BigDecimal.ZERO,
       1,
-      new ObjectId("123400000000000000000000")),
+      new HashMap<String,ObjectId>(){{
+        this.put("TSA C.A.T.S Program", new ObjectId("6737d90505484e688bdc9420"));
+      }}),
   PIDL$PA$PI$DUPLICATE(
       IdCategoryType.DRIVERS_LICENSE_PHOTO_ID,
       USStateOrCity.PA,
@@ -72,7 +91,9 @@ public enum ApplicationRegistry {
       ApplicationSubtype.DUPLICATE,
       BigDecimal.ZERO,
       1,
-      new ObjectId("123400000000000000000000")),
+      new HashMap<String,ObjectId>(){{
+        this.put("TSA C.A.T.S Program", new ObjectId("672871ed2de24d7c8ba75c30"));
+      }}),
   PIDL$PA$PI$RENEWAL(
       IdCategoryType.DRIVERS_LICENSE_PHOTO_ID,
       USStateOrCity.PA,
@@ -80,7 +101,9 @@ public enum ApplicationRegistry {
       ApplicationSubtype.RENEWAL,
       BigDecimal.ZERO,
       1,
-      new ObjectId("123400000000000000000000")),
+          new HashMap<String,ObjectId>(){{
+            this.put("TSA C.A.T.S Program", new ObjectId("6720718f17d3b63a60456d7c"));
+          }}),
   PIDL$PA$PI$CHANGE_OF_ADDRESS(
       IdCategoryType.DRIVERS_LICENSE_PHOTO_ID,
       USStateOrCity.PA,
@@ -88,7 +111,7 @@ public enum ApplicationRegistry {
       ApplicationSubtype.CHANGE_OF_ADDRESS,
       BigDecimal.ZERO,
       1,
-      new ObjectId("123400000000000000000000")),
+          new HashMap<String,ObjectId>()),
   PIDL$PA$DL$INITIAL(
       IdCategoryType.DRIVERS_LICENSE_PHOTO_ID,
       USStateOrCity.PA,
@@ -96,7 +119,7 @@ public enum ApplicationRegistry {
       ApplicationSubtype.INITIAL,
       BigDecimal.ZERO,
       1,
-      new ObjectId("123400000000000000000000")),
+          new HashMap<String,ObjectId>()),
   PIDL$PA$DL$DUPLICATE(
       IdCategoryType.DRIVERS_LICENSE_PHOTO_ID,
       USStateOrCity.PA,
@@ -104,7 +127,7 @@ public enum ApplicationRegistry {
       ApplicationSubtype.DUPLICATE,
       BigDecimal.ZERO,
       1,
-      new ObjectId("123400000000000000000000")),
+          new HashMap<String,ObjectId>()),
   PIDL$PA$DL$RENEWAL(
       IdCategoryType.DRIVERS_LICENSE_PHOTO_ID,
       USStateOrCity.PA,
@@ -112,7 +135,7 @@ public enum ApplicationRegistry {
       ApplicationSubtype.RENEWAL,
       BigDecimal.ZERO,
       1,
-      new ObjectId("123400000000000000000000")),
+          new HashMap<String,ObjectId>()),
   PIDL$PA$DL$CHANGE_OF_ADDRESS(
       IdCategoryType.DRIVERS_LICENSE_PHOTO_ID,
       USStateOrCity.PA,
@@ -120,7 +143,29 @@ public enum ApplicationRegistry {
       ApplicationSubtype.CHANGE_OF_ADDRESS,
       BigDecimal.ZERO,
       1,
-      new ObjectId("123400000000000000000000"));
+      new HashMap<String,ObjectId>()),
+
+  BC$MD$INITIAL(
+        IdCategoryType.BIRTH_CERTIFICATE,
+        USStateOrCity.MD,
+        Optional.empty(),
+        ApplicationSubtype.INITIAL,
+        BigDecimal.ZERO,
+        1,
+        new HashMap<String,ObjectId>(){{
+          this.put("TSA C.A.T.S Program", new ObjectId("6720679117d3b63a60456c33"));
+        }}),
+
+  BC$NJ$INITIAL(
+          IdCategoryType.BIRTH_CERTIFICATE,
+          USStateOrCity.NJ,
+          Optional.empty(),
+          ApplicationSubtype.INITIAL,
+          BigDecimal.ZERO,
+          1,
+          new HashMap<String,ObjectId>(){{
+            this.put("TSA C.A.T.S Program", new ObjectId("672877fc2de24d7c8ba75c95"));
+          }});
 
   private final IdCategoryType idCategoryType;
   private final USStateOrCity usState;
@@ -128,7 +173,7 @@ public enum ApplicationRegistry {
   private final Optional<PIDLSubtype> pidlSubtype;
   private final BigDecimal amount;
   private final int numWeeks;
-  private final ObjectId blankFormId;
+  private final Map<String,ObjectId> orgsToFormIds;
 
   ApplicationRegistry(
       IdCategoryType idCategoryType,
@@ -137,22 +182,21 @@ public enum ApplicationRegistry {
       ApplicationSubtype applicationSubtype,
       BigDecimal amount,
       int numWeeks,
-      ObjectId blankFormId) {
+      Map<String,ObjectId> orgsToFormsIds) {
     this.idCategoryType = idCategoryType;
     this.usState = usState;
     this.applicationSubtype = applicationSubtype;
     this.pidlSubtype = pidlSubtype;
     this.amount = amount;
     this.numWeeks = numWeeks;
-    this.blankFormId = blankFormId;
+    this.orgsToFormIds = orgsToFormsIds;
   }
 
-  @Override
-  public String toString() {
-    return this.toJSON().toString();
+  public String toString(String org) {
+    return this.toJSON(org).toString();
   }
 
-  public JSONObject toJSON() {
+  public JSONObject toJSON(String org) {
     JSONObject jsonObject = new JSONObject();
     jsonObject.put("idCategoryType", this.idCategoryType.toString());
     jsonObject.put("usState", this.usState.toString());
@@ -160,7 +204,7 @@ public enum ApplicationRegistry {
     jsonObject.put("pidlSubtype", this.pidlSubtype.map(PIDLSubtype::toString).orElse(""));
     jsonObject.put("amount", this.amount.toString());
     jsonObject.put("numWeeks", this.numWeeks);
-    jsonObject.put("blankFormId", this.blankFormId.toString());
+    jsonObject.put("blankFormId", this.orgsToFormIds.get(org).toString());
     return jsonObject;
   }
 }
